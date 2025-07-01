8366e0bc56e14e390b9edc32739be4c240419207
Anna De Lise <anna_delise@libero.it>
2025-07-01 17:33:29 +0200

    add first extra

---
+++ script/js.js
@@ -0,0 +1,21 @@
+// ALERT CONTA VIAGGI
+const viaggi = document.querySelectorAll("#miaSezione2 .col");
+const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    "</div>",
+  ].join("");
+
+  alertPlaceholder.append(wrapper);
+};
+
+const alertTrigger = document.getElementById("liveAlertBtn");
+if (alertTrigger) {
+  alertTrigger.addEventListener("click", () => {
+    appendAlert(`Viaggi disponibili: ${viaggi.length}`, "success");
+  });
+}
